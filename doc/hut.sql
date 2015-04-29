0a5e09dc4f89c5599bb96101fca0edc52e7a4921
Harald Hartmann <osm@haraldhartmann.de>
2015-04-29 20:55:00 +0200

    hut: update doc and sql script

--- doc/hut.sql
+++ doc/hut.sql
@@ -8,6 +8,8 @@ USE `osmpoll` ;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
@@ -22,6 +24,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut_image`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut_image` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_image` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `hut_id` INT(11) NOT NULL,
@@ -39,16 +43,19 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut_tags`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut_tags` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_tags` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `hut_id` INT(11) NOT NULL,
-  `key` VARCHAR(255) NOT NULL,
-  `value` VARCHAR(255) NOT NULL,
+  `tagkey` VARCHAR(255) NOT NULL,
+  `tagvalue` VARCHAR(255) NOT NULL,
   `created_by` VARCHAR(255) NOT NULL,
   `created_osmid` INT(11) NOT NULL,
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   INDEX `fk_hut_tags_1_idx` (`hut_id` ASC),
+  UNIQUE INDEX `tagkey_tagvalue_UNIQUE` (`tagkey` ASC, `tagvalue` ASC),
   CONSTRAINT `fk_hut_tags_1`
     FOREIGN KEY (`hut_id`)
     REFERENCES `osmpoll`.`hut` (`id`)
@@ -60,6 +67,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut_tags_voting`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut_tags_voting` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_tags_voting` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `hut_tags_id` INT(11) NOT NULL,
@@ -69,6 +78,7 @@ CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_tags_voting` (
   `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   INDEX `fk_hut_tags_voting_1_idx` (`hut_tags_id` ASC),
+  UNIQUE INDEX `hut_tags_id_created_osmid_UNIQUE` (`hut_tags_id` ASC, `created_osmid` ASC),
   CONSTRAINT `fk_hut_tags_voting_1`
     FOREIGN KEY (`hut_tags_id`)
     REFERENCES `osmpoll`.`hut_tags` (`id`)
@@ -80,6 +90,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut_tags_comment`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut_tags_comment` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_tags_comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `hut_tags_id` INT(11) NOT NULL,
@@ -100,6 +112,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `osmpoll`.`hut_comment`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `osmpoll`.`hut_comment` ;
+
 CREATE TABLE IF NOT EXISTS `osmpoll`.`hut_comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `hut_id` INT(11) NOT NULL,
