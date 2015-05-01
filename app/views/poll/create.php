cdf8397b31da5b3c24678dd11d3ded8aaa535a6d
Harald Hartmann <osm@haraldhartmann.de>
2015-05-01 16:36:16 +0200

    (github#9) fix comment function in create poll view

--- app/views/poll/create.php
+++ app/views/poll/create.php
@@ -39,12 +39,13 @@
                 <?=core\language::show('btn_start_hint','poll', \helpers\session::get('language')) ?>
               </div>
             </form>
+              <?php if(isset($data['question']) && $data['question']->id > 0) { ?>
               <div class="panel panel-default">
                 <div class="panel-heading">
                   <?=core\language::show('comment_headline','comment', \helpers\session::get('language')) ?>
                 </div>
                 <div class="panel-body">
-                <?php foreach ($data['comments'] as $comment) { ?>
+                <?php if(isset($data['comments'])) foreach ($data['comments'] as $comment) { ?>
                   <?=htmlentities($comment->created_by) ?> - <?=date_format(date_create($comment->created),'d. F Y H:i')?></strong>
                   <div class="well well-sm"><?=nl2br(htmlentities($comment->comment)) ?></div>
                   <hr>
@@ -56,9 +57,9 @@
                     <button type="submit" class="btn btn-default" name="submit" ><?=core\language::show('btn_create','comment', \helpers\session::get('language')) ?></button>
                   </div>
                 </form>
-
                 </div>
               </div>
+              <?php } ?>
           </div>
         </div>
       </div>
